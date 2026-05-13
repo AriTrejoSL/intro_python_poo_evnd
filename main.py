14dadcbf95e1dd08b93ae02fc704c186ba300c4c
AriTrejo <arianatrejo83@gmail.com>
2026-05-13 06:43:22 -0600

    primer practica, suma de dos numeros

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ def main():
 if __name__=="__main__":
     main()
 
+def suma():
+    numero1=int(input("Ingresa el primer número para la suma: "))
+    numero2=int(input("Ingresa el segundo número para la suma: "))
+    sumaf=numero1+numero2
+    print("La suma de dos números es de ", sumaf)
+
+suma()
+
